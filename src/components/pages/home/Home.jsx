0bfe18bfa2e47b93c0a9243f7897f41599ef5718
dev-rakibul1 <fl.rakibul@gmail.com>
2022-11-24 01:13:45 +0600

    banner hero done

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Hero from "./hero/Hero";
+const heroImages = "https://i.ibb.co/0smJFsf/bannar.jpg";
 
 const Home = () => {
   return (
     <div className="">
-      <section className="px-2">
+      <section
+        className="px-2  bg-opacity-60 bg-black banner-hero"
+        style={{ backgroundImage: `url(${heroImages})` }}
+      >
         <div className="md:w-[90%] mx-auto px-2">
           <Hero />
         </div>
--- src/components/pages/home/hero/Hero.jsx
+++ src/components/pages/home/hero/Hero.jsx
@@ -1,21 +1,24 @@
 import React from "react";
+import { FaAngleRight } from "react-icons/fa";
 
 const Hero = () => {
   return (
-    <div
-      className="hero min-h-screen"
-      style={{ backgroundImage: `url("https://placeimg.com/1000/800/arch")` }}
-    >
-      <div className="hero-overlay bg-opacity-60"></div>
+    <div className="hero min-h-screen">
+      {/* <div className="hero-overlay"></div> */}
       <div className="hero-content text-center text-neutral-content">
         <div className="max-w-md">
-          <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+          <h1 className="mb-5 text-5xl font-bold hero-title">
+            Choice your best car from gallery
+          </h1>
           <p className="mb-5">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi.
+            Claritas est etiam processus dynamicus, qui sequitur mutationem
+            consuetudium lectorum. Mirum est notare quam littera gothica, quam
+            nunc putamus parum claram.
           </p>
-          <button className="btn btn-primary">Get Started</button>
+          <button className="btn btn-secondary rounded-full btn-md">
+            View details
+            <FaAngleRight className="text-center ml-3 font-2xl" />
+          </button>
         </div>
       </div>
     </div>
