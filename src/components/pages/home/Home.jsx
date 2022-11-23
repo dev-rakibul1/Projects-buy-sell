e58efedd30349999bcf4c1e9ebdac219c5ca3949
dev-rakibul1 <fl.rakibul@gmail.com>
2022-11-24 04:27:26 +0600

    car section done

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -1,16 +1,88 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Micro from "../micro/Micro";
+import Car from "./../car/Car";
 import Hero from "./hero/Hero";
 const heroImages = "https://i.ibb.co/0smJFsf/bannar.jpg";
+const car1 = "https://i.ibb.co/Hn77h5R/car3.jpg";
+const car2 = "https://i.ibb.co/1vrdhZ6/car4.jpg";
+const car3 = "https://i.ibb.co/mzWnxWp/car2.jpg";
 
 const Home = () => {
+  const [someCars, setSomeCars] = useState([]);
+
+  // const { data: car, isLoading } = useQuery({
+  //   queryKey: ["car"],
+  //   queryFn: async () => {
+  //     const res = await fetch("http://localhost:5000/car-something");
+  //     const data = await res.json();
+  //     return data;
+  //   },
+  // });
+
+  // if (isLoading) {
+  //   return <Spinner />;
+  // }
+
+  useEffect(() => {
+    fetch("http://localhost:5000/car-something")
+      .then((res) => res.json())
+      .then((data) => setSomeCars(data))
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
     <div className="">
+      {/* hero banner */}
       <section
         className="px-2  bg-opacity-60 bg-black banner-hero"
         style={{ backgroundImage: `url(${heroImages})` }}
       >
         <div className="md:w-[90%] mx-auto px-2">
           <Hero />
+        </div>
+      </section>
+
+      {/* car */}
+      <section className="px-2 mt-16">
+        <div className="md:w-[90%] mx-auto px-2">
+          <div className="md:flex">
+            <div className="md:w-[30%] px-2">
+              <img src={car2} alt="" className="rounded-xl" />
+            </div>
+            <div className="md:w-[70%] px-2">
+              <div className="md:flex justify-between items-center py-7">
+                <span className="text-2xl font-bold">Most popular car</span>
+                <button className="font-semibold">See All</button>
+              </div>
+              <div className=" grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                {someCars.map((car) => (
+                  <Car key={car._id} car={car} />
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* micro */}
+      <section className="px-2 mt-16">
+        <div className="md:w-[90%] mx-auto px-2">
+          <div className="md:flex">
+            <div className="md:w-[30%] px-2">
+              <img src={car2} alt="" className="rounded-xl" />
+            </div>
+            <div className="md:w-[70%] px-2">
+              <div className="md:flex justify-between items-center py-7">
+                <span className="text-2xl font-bold">Most popular car</span>
+                <button className="font-semibold">See All</button>
+              </div>
+              <div className=" grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                {someCars.map((car) => (
+                  <Micro key={car._id} car={car} />
+                ))}
+              </div>
+            </div>
+          </div>
         </div>
       </section>
     </div>
---
+++ src/components/pages/micro/Micro.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+const Micro = ({ car }) => {
+  const {
+    title,
+    images,
+    yearsOfUse,
+    model,
+    speed,
+    resalePrice,
+    description,
+    color,
+  } = car;
+  return (
+    <div>
+      <div className="card card-compact w-full h-full border border-gray-200 rounded-md">
+        <figure>
+          <img src={images} alt="car" className="max-w-60 " />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title text-base font-bold text-secondary">
+            {title}
+          </h2>
+          <div>
+            <small className="block font-semibold text-sm">
+              Model: {model}
+            </small>
+            <small className="block font-semibold text-sm">
+              Speed: {speed}
+            </small>
+            <small className="block font-semibold text-sm">
+              Color: {color}
+            </small>
+          </div>
+          <p>{description.substring(0, 50)} [...]</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Micro;
